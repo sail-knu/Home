b7cbfaf28f16d1f8e719754f9192b85086a5d48a
CK <leeck@kongju.ac.kr>
2026-09-08 11:26:10 +0900

    vbcv

--- script.js
+++ script.js
@@ -493,7 +493,7 @@
 
   function courseDemoUrl(src) {
     const url = new URL(encodeURI(src), document.baseURI);
-    url.searchParams.set("v", "20260907d");
+    url.searchParams.set("v", "20260908");
     return url.href;
   }
 
